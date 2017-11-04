73b123818e31caf60d1dea88edb82559165cca84
Nicholas Cobb <ncobb314@gmail.com>
2017-11-04 15:57:05 -0500

    Fixes to index page for button handling and storage handling

---
+++ src/index.js
@@ -0,0 +1,70 @@
+var name;
+var address;
+var dateOfBirth;
+
+$(document).ready(function() {
+  name = localStorage.getItem("betoatsume-name");
+  address = localStorage.getItem("betoatsume-address");
+  dateOfBirth = localStorage.getItem("betoatsume-dob");
+  
+  console.log(name);
+  console.log(address);
+  console.log(dateOfBirth);
+  
+  if (name && address && dateOfBirth) {
+    window.location.href = "office.html";
+  }
+  
+  $("#continuebutton").prop('disabled', true);
+  
+  //TODO: check if dob field is well-formatted, not just truthy,
+
+  var buttonChange = function() {
+    if ($("#nameinput").val() && $("#addressinput").val() && checkDateFormatting($("#dateinput").val())) {
+      $("#continuebutton").prop('disabled', false);
+    } else {
+      $("#continuebutton").prop('disabled', true);
+    }
+  }
+ 
+  $("#nameinput").on("input", buttonChange);
+  $("#addressinput").on("input", buttonChange);
+  $("#dateinput").on("input", buttonChange);
+  
+  continueButton = document.getElementById('continuebutton');
+  continueButton.addEventListener('click', function() {
+    name = localStorage.setItem("betoatsume-name", $("#nameinput").val());
+    address = localStorage.setItem("betoatsume-address", $("#addressinput").val());
+    dateOfBirth = localStorage.setItem("betoatsume-dob", $("#dateinput").val());
+    window.location.href = "office.html";
+  }, false);
+})
+
+function checkDateFormatting(date) {
+  var dateFields = date.split('/');
+  if (dateFields.length != 3) {
+    return false;
+  }
+  
+  var month = parseInt(dateFields[0]);
+  var day = parseInt(dateFields[1]);
+  var year = parseInt(dateFields[2]);
+
+  return isValidDate(day, month, year);
+}
+
+function isValidDate(d, m, y) {
+    m = m - 1;
+    return m >= 0 && m < 12 && d > 0 && d <= daysInMonth(m, y) && y > 1900 && y < (new Date()).getFullYear();
+}
+
+function daysInMonth(m, y) {
+    switch (m) {
+        case 1 :
+            return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
+        case 8 : case 3 : case 5 : case 10 :
+            return 30;
+        default :
+            return 31
+    }
+}
