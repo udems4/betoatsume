cdb882f0d867f78264db31128deb97cf7f9658d6
Morgan Lawless <morganblawless@gmail.com>
2017-11-05 12:06:29 -0600

    cat tests

--- src/poll.js
+++ src/poll.js
@@ -1,18 +1,18 @@
 function getPollData(form){
   for (Count = 0; Count < 5; Count++) {
-    if (form.registration[Count].checked){
+    if (form.question1[Count].checked){
         localStorage.setItem("question1", Count);
         break;
       }
   }
   for (Count = 0; Count < 5; Count++) {
-    if (form.candidate[Count].checked){
+    if (form.question2[Count].checked){
         localStorage.setItem("question2", Count);
         break;
       }
   }
   for (Count = 0; Count < 5; Count++) {
-    if (form.candidate[Count].checked){
+    if (form.question3[Count].checked){
         localStorage.setItem("question3", Count);
         break;
       }
