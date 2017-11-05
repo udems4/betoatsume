7ea3944801e4adf664328751b44781f646e6e311
Nicholas Cobb <ncobb314@gmail.com>
2017-11-05 12:25:58 -0600

    add shop

--- src/spendpoints.js
+++ src/spendpoints.js
@@ -1,14 +1,19 @@
-var clout = parseInt(localStorage.getItem("betoatsume-clout"));
+var clout = 500;
 
-var removePointButton;
+var items = [
+  {name: "First Aid Kit", key: "firstaid", isOwned: false, cost: 250},
+  {name: "Bus", key: "bus", isOwned: false, cost: 400},
+  {name: "Bass", key: "bass", isOwned: false, cost: 600},
+  {name: "Jersey", key: "jersey", isOwned: false, cost: 700},
+  {name: "Books", key: "books", isOwned: false, cost: 800},
+  {name: "Tan Suit", key: "tansuit", isOwned: false, cost: 1000},
+  {name: "Infinite Jest", key: "infinitejest", isOwned: false, cost: 1250},
+];
 
 $(document).ready(function() {
+    loadItems();
+    addClickHandlers();
     setPointBalanceText();
-
-    removePointButton = document.getElementById('spendpoints');
-    removePointButton.addEventListener('click', function() {
-      changePoints(-1);
-    }, false);
  })
   
 function setPointBalanceText() {
@@ -20,5 +25,46 @@ function changePoints(deltaClout) {
     clout += deltaClout;
     localStorage.setItem("betoatsume-clout", String(clout));
     setPointBalanceText();
+    return true;
   }
+  return false;
+}
+
+function loadItems() {
+  for (i = 0; i < items.length; i++) {
+    if (localStorage.getItem("betoatsume-item-" + items[i].key) === "true") {
+      items[i].isOwned = true;
+    } else {
+      console.log("HERE");
+      $("#itemlist").append("<li id=\"item-" + items[i].key + "\" class=\"text-primary\">" + items[i].name + ": " + items[i].cost + " points</li>");
+    }
+  }
+}
+
+function addClickHandlers() {
+  $("#itemlist").children().each(function(index) {
+    console.log($(this));
+    var key = $(this).attr('id').substring(5);
+
+    $(this).click(function() {
+      var item = getItem(key);
+      var isCharged = changePoints(-1 * item.cost);
+
+      if (isCharged) {
+        localStorage.setItem("betoatsume-item-" + item.key, "true");
+        $(this).remove();
+      } else {
+        alert("You do not have enough points to buy this!");
+      }
+    });
+  });
+}
+
+function getItem(key) {
+  for (i = 0; i < items.length; i++) {
+    if (items[i].key === key) {
+      return items[i];
+    }
+  }
+  return null;
 }
