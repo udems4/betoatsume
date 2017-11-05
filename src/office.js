097b548187d974e29be3e545faa87d02ed6f8e7f
Nicholas Cobb <ncobb314@gmail.com>
2017-11-05 16:53:02 -0600

    aaaaaaaaaaaaa

--- src/office.js
+++ src/office.js
@@ -42,13 +42,9 @@ $(document).ready(function() {
   });
 
   $("#computer-box").click(function() {
-    showModal();
+    $("#computer-modal").modal();
   });
  })
-
-function showModal(){
-  $("#computer-modal").modal();
-}
 
 function setPointBalanceText() {
   $("#pointbalance").text("Point balance: " + clout);
@@ -97,6 +93,34 @@ function addCats() {
       $("#images").append("<img src=\"" + cats[i].image + "\" class=\"catimg\" id=\"cat-" + cats[i].key + "\">");
     }
   }
+  
+  $('.catimg').click(function() {
+    var key = $(this).attr('id').substring(4);
+    $("#candidate-modal").modal();
+    $("#modal-candidate-name").html(cats[getCatIndex(key)].name);
+    $("#modal-candidate-body").html(getCandidateBio(key));
+  });
+}
+
+function getCandidateBio(key) {
+  if (key === "betoorourke") {
+    return "<p>Likes: Bass Guitar, El Paso</p>\n<p>Dislikes: Ted Cruz</p>\n<p>Beto O'Rourke is a Democratic Congressman who represents Texas' 16th congressional district and is running to replace Ted Cruz in the U.S. Senate. A former Punk Bass guitarist, tech executive, and advocate he supports ending the War on Drugs, Campaign finance reform, and comprehensive immigration reform. Click here to learn more about Beto O'Rourke. https://betofortexas.com/</p>";
+  } else if (key === "annrichards") {
+    return "<p>Likes: Feminism, Well-funded Public Schools</p>\n<p>Dislikes: Karl Rove</p>\n<p>Ann Richards was Governor of Texas from 1990 to 1994. Born in Texas in 1933 right in the middle of the Depression she was deeply impacted by the New Deal and became a lifelong Democrat  During her tenure she passed bills to regulate the sale of firearms, fund public education, streamline government finances,and advance women's rights. Her Daughter Cecile is the president of Planned Parenthood and like her mother an outspoken defender of women's rights. Ann Richards passed away in 2006 after a lifetime of fighting for common people.</p>";
+  } else if (key === "barackobama") {
+    return "<p>Likes: Universal Healthcare, Community Organizing, Michelle, Thin Mints </p>\n<p>Dislikes: Neil Gorsuch, Watching the Bulls lose</p>\n<p>Barack Obama is a former Illinois Senator and President of the United States. He was born in Hawaii in 1961 and lived in Washington State and Indonesia. His tenure as president included landmark legislation including the Affordable Care act, Dodd Frank Wall Street  Reform and Consumer Protection Act and the Lily Ledbetter ir Pay Act. He was instrumental in the acceptance of the seminal Paris Climate Agreement. He nominated Sonya Sotomayor to the Supreme Court making her the first Hispanic justice. Click here to learn more about Barack Obama</p>\n<p>https://barackobama.com/</p>";
+  } else if (key === "lloyddoggett") {
+    return "<p>Likes: Medicare, Environment, Progressive Taxation</p>\n<p>Dislikes: Republicans</p>\n<p>Lloyd Doggett is a representative in the United States Congress representing Texas's 35th district. Previously he served as a Texas state Senator, and as a justice on the Texas State Supreme Court. Doggett authored the bill creating the Texas Commission on Human Rights, as well as a law outlawing \"cop killer\" bullets and a \"sunset law\" He is a strong advocate for working families and for more transparent governor. To Learn more about Lloyd Doggett click here https://www.votedoggett.com/</p>";
+  } else if (key === "ginahinojosa") {
+    return "<p>Likes: Education, Transportation/Infastructure, Women & Families</p>\n<p>Dislikes: Dan Patrick</p>\n<p>Gina Hinojosa is a member of the Texas Legislature representing District 49. She is a Texas native and has lived in Austin for 25 years. She is a strong advocate for education and children. Being a former member of the AISD School board she fought for increased public education spending. SHe is a strong supporter of organized labor and has fought to represent workers who have been discriminated against due to age, race, gender, and disability. To learn more about Gina Hinojosa click here http://www.ginaforaustin.com/</p>";
+  } else if (key === "donnahoward") {
+    return "<p>Likes: Healthcare</p>\n<p>Dislikes: Anti-Vaxxers</p>\n<p>Representative Donna Howard has served in the Texas House of Representatives since 2006. She is a member of the House Appropriations Committee, and sits on its Article III Subcommittee which oversees education spending in the state budget. Donna serves as Vice-Chair of the Calendars Committee, is also a longtime member of both the House Higher Education and House Administration Committees. A native of Austin, Donna earned a Bachelor's degree in nursing and a Master's degree in health education from the University of Texas. She worked as a critical care nurse at Brackenridge and Seton hospitals, and served as Austin's first hospital-based Patient Education Coordinator. Donna is a past president of the Texas Nurses Association (District 5) and was a health education instructor at UT.</p>";
+  } else if (key === "steveadler") {
+    return "<p>Likes: Bonds, Buses</p>\n<p>Dislikes: Traffic</p>\n<p>Steve Adler is the current mayor of Austin, Texas. He grew up in Washington, but came to the University of Texas for law school and has remained in Austin since. As a lawyer in Austin he frequently advocated for the rights of people who had been discriminated against. Since becoming mayor in 2015, he's helped to raise the minimum wage for city employees.</p>"
+  } else if (key === "dougsnyder") {
+    return "<p>Likes: Cheese & Crackers</p>\n<p>Dislikes: Institutional Discrimination</p>\n<p>Doug Snyder is the president of University Democrats at the University of Texas at Austin.</p>"
+  }
+  return "";
 }
 
 function updateCats() {
